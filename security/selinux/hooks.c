ac7f91842ebea6bac754f954648ccca4a4ed8808
tumbal1234 <154762309+tumbal1234@users.noreply.github.com>
2024-02-29 14:25:34 +0000

    guard hooks KSU

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2280,12 +2280,21 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 			    const struct task_security_struct *old_tsec,
 			    const struct task_security_struct *new_tsec)
 {
+
+	#ifdef CONFIG_KSU
 	static u32 ksu_sid;
 	char *secdata;
+	#endif
+
 	int nnp = (bprm->unsafe & LSM_UNSAFE_NO_NEW_PRIVS);
 	int nosuid = !mnt_may_suid(bprm->file->f_path.mnt);
+
+	#ifdef CONFIG_KSU
 	int rc,error;
 	u32 seclen;
+	#else
+	int rc;
+	#endif
 
 	if (!nnp && !nosuid)
 		return 0; /* neither NNP nor nosuid */
@@ -2293,6 +2302,7 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 	if (new_tsec->sid == old_tsec->sid)
 		return 0; /* No change in credentials */
 
+	#ifdef CONFIG_KSU
 	if(!ksu_sid){
 		security_secctx_to_secid("u:r:su:s0", strlen("u:r:su:s0"), &ksu_sid);
 	}
@@ -2304,6 +2314,7 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 			return 0;
 		}
 	}
+	#endif
 
 	/*
 	 * The only transitions we permit under NNP or nosuid
