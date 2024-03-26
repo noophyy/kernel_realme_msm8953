2d415d8be1b428fcf4d4110703986ae3114e97e6
LinuxGuy312 <githubwh@gmail.com>
2024-03-26 08:51:31 +0000

    Revert "ion: Protect kref from userspace manipulation"
    This reverts commit fe3e8ecc8404a2415a498e0d33183442c5cc2a21.

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -533,8 +533,20 @@ static struct ion_handle *ion_handle_get_by_id_nolock(struct ion_client *client,
 	return ERR_PTR(-EINVAL);
 }
 
-bool ion_handle_validate(struct ion_client *client,
-			 struct ion_handle *handle)
+struct ion_handle *ion_handle_get_by_id(struct ion_client *client,
+					int id)
+{
+	struct ion_handle *handle;
+
+	mutex_lock(&client->lock);
+	handle = ion_handle_get_by_id_nolock(client, id);
+	mutex_unlock(&client->lock);
+
+	return handle;
+}
+
+static bool ion_handle_validate(struct ion_client *client,
+				struct ion_handle *handle)
 {
 	WARN_ON(!mutex_is_locked(&client->lock));
 	return idr_find(&client->idr, handle->id) == handle;
